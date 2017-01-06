e162e42b137f23f96466bb62fb238e1a54d39691
Dave J <chronus@protonmail.com>
2017-01-06 13:21:33 +0100

    adding fancy constructor for rocksdb_storage

--- src/rocks_storage.cc
+++ src/rocks_storage.cc
@@ -1,12 +1,22 @@
-//
-// Created by florian on 1/5/17.
-//
+/* rocks_storage.h
+ * ACU Implementation
+ *
+ * <src/rocks_storage.cc>
+ */
 
 #include "rocks_storage.h"
 
 namespace beemaster {
+
+    /// Initialise and open DB
+    RocksStorage::RocksStorage(std::string db_name)
+        : acu::Storage::Storage(db_name) {
+            options.create_if_missing = true;
+            rocksdb::Status status = rocksdb::DB::Open(options, db_name, &Database);
+            assert(status.ok());
+        }
+
     void RocksStorage::Persist(const acu::IncomingAlert *alert) {
 
     }
 }
-
--- src/rocks_storage.h
+++ src/rocks_storage.h
@@ -1,17 +1,26 @@
-//
-// Created by florian on 1/5/17.
-//
+/* rocks_storage.h
+ * ACU Implementation
+ *
+ * Provide a interface to RocksDB as an implementation of
+ * abstract Storage class.
+ *
+ * @author: 1wilkens, 1jost
+ */
 
 #ifndef ACU_IMPL_ROCKSSTORAGE_H
 #define ACU_IMPL_ROCKSSTORAGE_H
 
 #include <acu/storage.h>
+#include <rocksdb/db.h>
 
 namespace beemaster {
 
     class RocksStorage : public acu::Storage {
     public:
-        RocksStorage(std::string db_name) : acu::Storage::Storage(db_name) {}
+        rocksdb::DB* Database;
+        rocksdb::Options options;
+
+        RocksStorage(std::string db_name);
 
         void Persist(const acu::IncomingAlert *alert);
     };
